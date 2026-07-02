20347a7ce352a32c8411c5d2c30a5d54ff6facf5
Rushikesh Chavan <157344215+rushikeshchavan-7@users.noreply.github.com>
2026-07-03 00:05:27 +0530

    [217] [Time Beats: 5.24%] [Memory Beats: 91.87%] - LeetPush

--- 217-contains-duplicate/217-Contains-Duplicate.cs
+++ 217-contains-duplicate/217-Contains-Duplicate.cs
@@ -1,13 +1,13 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
         nums.Sort();
-        for (int i = 0; i<nums.Length; i++)
+        for (int i = 1; i<nums.Length; i++)
         {
-            for (int j = i+1; j<nums.Length; j++)
+            if (nums[i]==nums[i-1])
             {
-                if (nums[i]==nums[j])
-                {return true;}
+                return true;
             }
+           
         }
         return false;
     }
