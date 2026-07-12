58abd527283f39c0ab0fc53ca5e6e1c7bb20423b
Rushikesh Chavan <157344215+rushikeshchavan-7@users.noreply.github.com>
2026-07-12 12:28:08 +0530

    [448] [Time Beats: 44.68%] [Memory Beats: 68.30%] - LeetPush

---
+++ 448-find-all-numbers-disappeared-in-an-array/448-Find-All-Numbers-Disappeared-in-an-Array.cs
@@ -0,0 +1,18 @@
+public class Solution
+{
+    public IList<int> FindDisappearedNumbers(int[] nums)
+    {
+        HashSet<int> set = new HashSet<int>(nums);
+        List<int> result = new List<int>();
+
+        for (int i = 1; i <= nums.Length; i++)
+        {
+            if (!set.Contains(i))
+            {
+                result.Add(i);
+            }
+        }
+
+        return result;
+    }
+}
